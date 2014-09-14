e4b7fa26e6f5e67f98fffaaf5e2f3f137f323e6e
derrickarteus <Arteus@Derricks-MacBook-Pro.local>
2014-09-14 15:10:25 -0700

    Commit for refactor lesson

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
    $('#submit').on('click', function() {
        // Store the number entered in the input field as input variable
        var input = $('#number').val();
-       // Start a for loop using input variable as the truth
+       // Start a for loop using input variable as the conditional check
        for (var i = 1; i <= input; i++) {
            // If number is divisible by both 3 and 5, append FizzBuzz to result div
            if (i % 3 === 0 && i % 5 === 0) {
@@ -14,7 +14,7 @@ $(document).ready(function() {
             // If number is divisible by 5, append Buzz to result div
            } else if (i % 5 === 0) {
                $('#result').append('Buzz' + '<p>');
-            // If number is not divisible by 3 or 5, append hte number to result div
+            // If number is not divisible by 3 or 5, append the number to result div
            } else {
                $('#result').append(i + '<p>');
            }
